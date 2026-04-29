db4e1706c3ac65e116853a228824571fa4664884
Diego Alejandro Londono <dalondonoh@unadvirtual.edu.co>
2026-04-29 17:18:32 -0500

    Mejoras de la clase abstracta

--- main.py
+++ main.py
@@ -7,26 +7,34 @@ class EntidadBase(ABC):
     def __init__(self):
         pass
 
+    @abstractmethod
     def salida(self):
         pass
 
+    @abstractmethod
     def ingreso(self):
         pass
 
+    @abstractmethod
     def precio(self):
         pass
 
+    @abstractmethod
     def reserva(self):
         pass
 
+    @abstractmethod
     def cancelar_reserva(self):
         pass
 
+    @abstractmethod
     def equipo(self):
         pass
 
+    @abstractmethod
     def tiempo(self):
         pass
 
+    @abstractmethod
     def descuento(self):
         pass
