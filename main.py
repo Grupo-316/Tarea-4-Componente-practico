e5d192f318a1e2ede07ff8c52be65955dfcfe680
Diego Alejandro Londono <dalondonoh@unadvirtual.edu.co>
2026-04-29 10:02:11 -0500

    Creacion de la clase abtracta

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-#aaaa
+from abc import ABC, abstractmethod
+
+
+class EntidadBase(ABC):
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    def salida(self):
+        pass
+
+    def ingreso(self):
+        pass
+
+    def precio(self):
+        pass
+
+    def reserva(self):
+        pass
+
+    def cancelar_reserva(self):
+        pass
+
+    def equipo(self):
+        pass
+
+    def tiempo(self):
+        pass
+
+    def descuento(self):
+        pass
