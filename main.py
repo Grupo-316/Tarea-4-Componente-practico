db5c4be857eede9520e2480da8a7ee81bb646253
Diego Alejandro Londono <dalondonoh@unadvirtual.edu.co>
2026-05-01 09:19:28 -0500

    Mejoras en la clase abtracta

--- main.py
+++ main.py
@@ -10,38 +10,6 @@ class EntidadBase(ABC):
         pass
 
     @abstractmethod
-    def salida(self):
-        pass
-
-    @abstractmethod
-    def ingreso(self):
-        pass
-
-    @abstractmethod
-    def precio(self):
-        pass
-
-    @abstractmethod
-    def reserva(self):
-        pass
-
-    @abstractmethod
-    def cancelar_reserva(self):
-        pass
-
-    @abstractmethod
-    def equipo(self):
-        pass
-
-    @abstractmethod
-    def tiempo(self):
-        pass
-
-    @abstractmethod
-    def descuento(self):
-        pass
-
-    @abstractmethod
     def validar_datos(self):
         pass
 
